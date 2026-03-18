15aec1a448615ea2a297a4f324ce66ef4900d1b1
devank <adreshsingh275127@gmail.com>
2026-03-18 08:30:04 +0530

    Not got it! Woop

---
+++ NexPermutation.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class NexPermutation {
+
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+
+        // Step 1: Find first decreasing element from right
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // Step 2: If found, swap with next greater element on right
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        // Step 3: Reverse the right part
+        reverse(nums, i + 1, n - 1);
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        NexPermutation sol = new NexPermutation();
+        sol.nextPermutation(nums);
+
+        System.out.println(Arrays.toString(nums));
+
+        scanner.close();
+    }
+}
