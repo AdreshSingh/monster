18a66bed072df6cd511847ec3cc5960bdc42b136
devank <adreshsingh275127@gmail.com>
2026-05-13 14:40:03 +0530

    fixed and removed unused lines

--- c-1/LearnSortingInHashMap.java
+++ c-1/LearnSortingInHashMap.java
@@ -2,7 +2,6 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class LearnSortingInHashMap {
